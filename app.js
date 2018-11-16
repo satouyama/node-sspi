144e525568abd5864ab9020628f3628ad9a65a8d
DOUGLAS LIMA DIAS <douglas.dias@centralit.com.br>
2018-11-16 10:04:55 -0200

    update

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(function(req, res, next) {
 })
 // Start server
 var port = process.env.PORT || 3000
+
 server.listen(port, function () {
   console.log('Express server listening on port %d in %s mode', port, app.get('env'))
 })
